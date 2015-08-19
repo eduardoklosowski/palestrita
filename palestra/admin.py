7909604bc9039eb5215ec84b4e9b6cd5a881da30
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2015-08-19 16:20:24 -0300

    Criado admin do "palestra"

--- palestra/admin.py
+++ palestra/admin.py
@@ -1,3 +1,45 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class TagInline(admin.TabularInline):
+    model = models.Tag
+    extra = 0
+    prepopulated_fields = {'slug': ('nome',)}
+
+
+@admin.register(models.TipoTag)
+class TipoTagAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'slug', 'cor')
+    list_display_links = ('nome', 'slug')
+    search_fields = ('nome', 'slug', '=cor')
+    prepopulated_fields = {'slug': ('nome',)}
+    inlines = [TagInline]
+
+
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('tipo', 'nome', 'slug')
+    list_display_links = ('nome', 'slug')
+    list_filter = ('tipo',)
+    search_fields = ('nome', 'slug')
+    prepopulated_fields = {'slug': ('nome',)}
+
+
+@admin.register(models.Palestrante)
+class PalestranteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'slug', 'has_foto', 'has_info')
+    list_display_links = ('nome', 'slug')
+    search_fields = ('nome', 'slug')
+    prepopulated_fields = {'slug': ('nome',)}
+
+
+@admin.register(models.Palestra)
+class PalestraAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'slug', 'url')
+    list_display_links = ('nome', 'slug')
+    search_fields = ('nome', 'slug', 'url')
+    prepopulated_fields = {'slug': ('nome',)}
--- palestra/models.py
+++ palestra/models.py
@@ -47,6 +47,16 @@ class Palestrante(models.Model):
     def __str__(self):
         return self.nome
 
+    def has_foto(self):
+        return bool(self.foto)
+    has_foto.boolean = True
+    has_foto.short_description = 'tem foto?'
+
+    def has_info(self):
+        return bool(self.info)
+    has_info.boolean = True
+    has_info.short_description = 'tem info?'
+
 
 @python_2_unicode_compatible
 class Palestra(models.Model):
