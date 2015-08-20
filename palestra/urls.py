1ddd8fbc2a031dbeafbcdbc9fc82ccaaf29a7336
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2015-08-20 10:32:01 -0300

    Criado view "palestra.IndexView"

--- palestra/urls.py
+++ palestra/urls.py
@@ -1,5 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 
+from . import views
+
+
+url_list = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+]
 
 urlpatterns = [
+    url(r'', include(url_list, namespace='palestra')),
 ]
--- palestra/views.py
+++ palestra/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+from django.views import generic
+
+from . import forms
+
+
+class PalestraPesquisaFormMixin(object):
+    def get_context_data(self, **kwargs):
+        context = super(PalestraPesquisaFormMixin, self).get_context_data(**kwargs)
+        context['palestra_pesquisa_form'] = forms.PalestraPesquisaForm(self.request.GET)
+        return context
+
+
+class IndexView(PalestraPesquisaFormMixin, generic.TemplateView):
+    template_name = 'palestra/index.html'
