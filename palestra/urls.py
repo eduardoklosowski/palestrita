bb5d14aafb0ab7524df3f74ee227bd9caf4e96b8
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2015-08-31 14:10:49 -0300

    Adicionado listagem de tags como página principal

--- palestra/urls.py
+++ palestra/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
+from django.views import generic
 
 from . import views
 
 
 url_list = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', generic.RedirectView.as_view(url='tag/', permanent=False), name='index'),
     url(r'^palestra/$', views.PalestraListView.as_view(), name='palestra_list'),
     url(r'^palestra/(?P<slug>[\w-]+)/$', views.PalestraDetailView.as_view(), name='palestra'),
     url(r'^tag/$', views.TipoTagListView.as_view(), name='tipotag_list'),
