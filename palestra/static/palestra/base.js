a88ee4ebdbdc2a9ce73f87ec5c6c86c8cd5310dd
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2015-08-31 14:52:53 -0300

    Mantem pesquisa avançada se tiver algum valor preenchido

--- palestra/static/palestra/base.js
+++ palestra/static/palestra/base.js
@@ -5,9 +5,15 @@ var palestra = {
   init_palestra_pesquisa_form: function() {
     var filtros = window.document.querySelectorAll('.palestra_pesquisa .avancada'),
       botoes = window.document.querySelectorAll('.palestra_pesquisa .avancado'),
-      i;
+      i,
+      filtro;
     for (i = filtros.length; i--;) {
-      filtros[i].classList.toggle('hide');
+      filtro = filtros[i];
+      if (filtro.querySelector('[name=tag]').value === '' &&
+          filtro.querySelector('[name=excludetag]').value === '' &&
+          filtro.querySelector('[name=palestrante]').value === '') {
+        filtro.classList.toggle('hide');
+      }
     }
     for (i = botoes.length; i--;) {
       botoes[i].addEventListener('click', this.palestra_pesquisa_form_avancado_toggle);
